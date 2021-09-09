488658915b572a7da0f5c1934a90c28ed584ede8
Mohammad Rizal Rizwan <rizalrizwan5@gmail.com>
2021-09-10 02:43:12 +0700

    Create phpembedhtml.php

---
+++ phpembedhtml.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Ini adalah sebuah skrip HTML</title>
+    </head>
+    <body>
+
+        <?php
+            echo "Halo semua! Aku adalah skrip PHP!";
+        ?>
+
+    </body>
+</html>
